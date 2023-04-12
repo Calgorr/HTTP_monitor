d53ae8f40b8b95857fdd2f02b81badec3f3a9222
calgor <ahomayoni684@gmail.com>
2023-04-12 11:51:07 +0330

    added New user fun

--- Model/user.go
+++ Model/user.go
@@ -1,5 +1,15 @@
 package model
 
+import "errors"
+
 type User struct {
 	Username, Password string
+	URLs               []URL
+}
+
+func NewUser(username, password string) (*User, error) {
+	if len(password) == 0 || len(username) == 0 {
+		return nil, errors.New("username or password can not be empty")
+	}
+	return &User{Username: username, Password: password}, nil
 }
